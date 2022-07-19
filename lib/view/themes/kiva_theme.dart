84d43bfc8d85da9724a24599ffd214f81f7c35a4
Gregory ULM <gregory.ulm@me.com>
2022-07-19 13:06:35 +1100

    Added buttons. Refactoring buttons with parameters.

--- lib/view/themes/kiva_theme.dart
+++ lib/view/themes/kiva_theme.dart
@@ -2,14 +2,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_kiva_theme/view/themes/kiva_theme_const.dart';
 
+//Enum
+enum typeButton { classic, round }
+
 class KivaTheme {
   //Colors
   ThemeData themedata = ThemeData(
     appBarTheme: const AppBarTheme(
       color: kMainColor,
     ),
-    textButtonTheme: TextButtonThemeData(
-      style: TextButton.styleFrom(
+    outlinedButtonTheme: OutlinedButtonThemeData(
+      style: OutlinedButton.styleFrom(
         primary: kMainColor,
       ),
     ),
@@ -24,7 +27,88 @@ class KivaTheme {
         backgroundColor: kFloatActionButtonColor,
         hoverColor: kFloatActionButtonHoverColor),
     checkboxTheme: CheckboxThemeData(
-      fillColor: MaterialStateProperty.all(Colors.orange[500]),
-    ),
+        fillColor: MaterialStateProperty.all(kCheckBoxFillColor),
+        overlayColor: MaterialStateProperty.all(kCheckBoxOverlayColor),
+        shape: const CircleBorder()),
   );
+
+  //Widget
+  //OutlinedButton
+//TODO Finish refactacoring the buttons.
+  OutlinedButton KivaButtonCfg(
+      VoidCallback logic, String data, typeButton selected) {
+    return OutlinedButton(
+      onPressed: logic,
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(kMainColor),
+        fixedSize: MaterialStateProperty.all(
+            const Size(kMainButtonWidth, kMainButtonHeight)),
+        shape: selected == typeButton.classic
+            ? MaterialStateProperty.all(RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)))
+            : null,
+      ),
+      child: Center(
+        child: Text(
+          data,
+          style: const TextStyle(color: kBodyColorText),
+        ),
+      ),
+    );
+  }
+
+  OutlinedButton KivaDefaultButton(VoidCallback logic, String data) {
+    return OutlinedButton(
+      onPressed: logic,
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(kMainColor),
+        fixedSize: MaterialStateProperty.all(
+            const Size(kMainButtonWidth, kMainButtonHeight)),
+        shape: null,
+      ),
+      child: Center(
+        child: Text(
+          data,
+          style: const TextStyle(color: kBodyColorText),
+        ),
+      ),
+    );
+  }
+
+  OutlinedButton KivaMenuButton(VoidCallback logic, String data) {
+    return OutlinedButton(
+      onPressed: logic,
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(kMainColor),
+        fixedSize: MaterialStateProperty.all(
+            const Size(kMainButtonWidth, kMainButtonHeight)),
+        shape: null,
+      ),
+      child: Center(
+        child: Text(
+          data,
+          style: const TextStyle(color: kBodyColorText),
+        ),
+      ),
+    );
+  }
+
+  OutlinedButton KivaMenuRoundButton(VoidCallback logic, String data) {
+    return OutlinedButton(
+      onPressed: logic,
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(kMainColor),
+        fixedSize: MaterialStateProperty.all(
+            const Size(kMainButtonWidth, kMainButtonHeight)),
+        shape: MaterialStateProperty.all(
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0))),
+      ),
+      child: Center(
+        child: Text(
+          data,
+          style: const TextStyle(color: kBodyColorText),
+        ),
+      ),
+    );
+  }
 }
--- lib/view/themes/kiva_theme_const.dart
+++ lib/view/themes/kiva_theme_const.dart
@@ -1,8 +1,15 @@
-//This file will hold all values for Colors
+//This file will hold all values
 import 'package:flutter/material.dart';
 
+//Colors
 const Color kMainColor = Colors.orange;
 const Color kAppBackgroundColor = Color.fromARGB(255, 227, 224, 220);
 const Color kBodyColorText = Colors.black;
 const Color kFloatActionButtonColor = Color.fromARGB(255, 255, 145, 0);
 const Color kFloatActionButtonHoverColor = Color.fromARGB(255, 255, 200, 133);
+const Color kCheckBoxFillColor = Color.fromARGB(255, 255, 132, 0);
+const Color kCheckBoxOverlayColor = Colors.black;
+
+//Widths and Heights
+const double kMainButtonWidth = 150.0;
+const double kMainButtonHeight = 60.0;
